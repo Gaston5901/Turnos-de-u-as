77136745499feccd58b53c12b07fd5374a5a8eef
Gaston5901 <gastonituarte100@gmail.com>
2026-02-06 00:32:53 -0300

    probando conexion

--- src/hooks/useCarruselImages.js
+++ src/hooks/useCarruselImages.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import { API_BASE_URL } from '../config/apiBaseUrl.js';
 
 // Hook para obtener imágenes del carrusel desde la API
@@ -6,10 +7,32 @@ export default function useCarruselImages() {
   const [imagenes, setImagenes] = useState([]);
 
   useEffect(() => {
+    let avisoTimeout = null;
+    let avisoMostrado = false;
+
+    avisoTimeout = setTimeout(() => {
+      avisoMostrado = true;
+      toast.info('Conexion lenta. Estamos cargando el carrusel...', { autoClose: 4000 });
+    }, 4000);
+
     fetch(`${API_BASE_URL}/carrusel`)
       .then(res => res.json())
       .then(data => setImagenes(Array.isArray(data.imagenes) ? data.imagenes : []))
-      .catch(() => setImagenes([]));
+      .catch(() => setImagenes([]))
+      .finally(() => {
+        if (avisoTimeout) {
+          clearTimeout(avisoTimeout);
+        }
+        if (avisoMostrado) {
+          toast.dismiss();
+        }
+      });
+
+    return () => {
+      if (avisoTimeout) {
+        clearTimeout(avisoTimeout);
+      }
+    };
   }, []);
 
   return imagenes;
